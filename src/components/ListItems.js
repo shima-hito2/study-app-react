d7a7e5ec37efd8d42ecad4cb9f5cab9c9cd17274
shimaoka hitoshi <shimaoka1203@gmail.com>
2024-01-18 19:01:00 +0900

    LINK修正

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -5,27 +5,18 @@ import ListItemText from '@mui/material/ListItemText'
 
 import DashboardIcon from '@mui/icons-material/Dashboard'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+import { Link } from 'react-router-dom'
 
 const MainListItems = () => {
 	return (
 		<React.Fragment>
-			<ListItemButton
-				sx={{ pr: 8 }}
-				onClick={() => {
-					window.location.href = '/admin/subject'
-				}}
-			>
+			<ListItemButton sx={{ pr: 8 }} component={Link} to='/admin/subject'>
 				<ListItemIcon>
 					<DashboardIcon />
 				</ListItemIcon>
 				<ListItemText primary='科目マスタ' />
 			</ListItemButton>
-			<ListItemButton
-				sx={{ pr: 8 }}
-				onClick={() => {
-					window.location.href = '/admin/regist'
-				}}
-			>
+			<ListItemButton sx={{ pr: 8 }} component={Link} to='/admin/regist'>
 				<ListItemIcon>
 					<ShoppingCartIcon />
 				</ListItemIcon>
